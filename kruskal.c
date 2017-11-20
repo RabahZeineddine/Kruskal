514502155e9cf5cbc95b04d7f47355f11f97f407
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2017-11-20 15:29:19 -0200

    Create Graph method added

--- kruskal.c
+++ kruskal.c
@@ -21,6 +21,28 @@ typedef struct grafo{
 	Aresta *arestas;
 }Grafo;
 
+void criarGrafo(Grafo *G,int V,int A);
+
+
+
+void criarGrafo(Grafo *G,int V,int A){
+	int i;
+	Aresta* arestas;
+	
+	arestas = (Aresta*) malloc(sizeof(Aresta) * A);
+	
+	for(i = 0; i < A; i++){
+		arestas[i].nome = i;
+		arestas[i].origem = -1;
+		arestas[i].destino = 999;
+		arestas[i].custo = 0;
+	}
+	
+	(*G).V = V;
+	(*G).A = A;
+	(*G).arestas = arestas;
+}
+
 int main(int argc,char *argv[]){
 /* Iremos resolver o exercicio com vertices e arestas a seguir */
 /*              V1
@@ -49,5 +71,8 @@ int main(int argc,char *argv[]){
 	
 	Grafo G;
 	V = 7; A = 9;
+	
+	criarGrafo(&G,V,A); /* Criar e inicializar o Grafo vazio */
+	
 
 }
